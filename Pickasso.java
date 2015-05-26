bf6d4fb569f570f725badc06d2334dc224b3f1f4
Connor <connor_333@hotmail.co.uk>
2015-05-26 10:54:50 +0100

    Fixed sometimes missing leading 0's

--- Pickasso.java
+++ Pickasso.java
@@ -2,30 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * simple colour picker tool */
+ * simple colour picker tool 
+ * that displays the hexadecimal and RGB representation of the colour the mouse if hovering over */
 public class Pickasso
 {
 	private JFrame frame;
 	private JPanel mainPanel;
 	private JLabel colourLabel;
-	private Robot robot;
+	private Robot robot; // this contains most of the functionality
+	private Point mousePos;
+	/** the colour where the mouse currently is */
+	private Color currentCol;
 
 	public Pickasso()
 	{
-		frame = new JFrame("Pickasso");
-		frame.setSize(160, 100);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-
-		mainPanel = new JPanel();
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
-		frame.add(mainPanel);
-
-		colourLabel = new JLabel();
-		mainPanel.add(colourLabel);
-		colourLabel.setHorizontalAlignment(JLabel.CENTER);
-		colourLabel.setFont(new Font("monospaced", Font.PLAIN, 14));
-		
 		try
 		{
 			robot = new Robot();
@@ -36,8 +26,29 @@ public class Pickasso
 			System.exit(-1);
 		}
 
-		frame.setVisible(true);
+		initialiseInterface();
 		startDetection();
+	}
+
+	/**
+	 * sets up the interface and its components */
+	private void initialiseInterface()
+	{
+		frame = new JFrame("Pickasso");
+		frame.setSize(160, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setAlwaysOnTop(true);
+
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+		frame.add(mainPanel);
+
+		colourLabel = new JLabel();
+		mainPanel.add(colourLabel);
+		colourLabel.setHorizontalAlignment(JLabel.CENTER);
+		colourLabel.setFont(new Font("monospaced", Font.PLAIN, 14));
+		frame.setVisible(true);
 	}
 
 	/**
@@ -52,33 +63,55 @@ public class Pickasso
 	 * sets the data for each component */
 	private void setData()
 	{
-		Color currentCol = getCurrentColour();
+		mousePos = MouseInfo.getPointerInfo().getLocation();
+		currentCol = getCurrentColour();
+
 		mainPanel.setBackground(currentCol);
 
-		Color contrast = getContrasting(currentCol);
-		colourLabel.setForeground(contrast);
-		colourLabel.setText(getColourString(currentCol).toUpperCase());
+		Color contrast = getContrasting();
+		colourLabel.setForeground(contrast); // makes sure we can read the text against the panels background
+		colourLabel.setText(getColourString());
 	}
 
 	/** 
 	 * used to make sure the text is readable against the main panels background
 	 * @param col the colour where the mouse is
 	 * @return white/black colour depending on the background of the main panel */
-	private Color getContrasting(Color col)
+	private Color getContrasting()
 	{
-		int balance;
-		double temp = 1 - (0.3 * col.getRed() + 0.6 * col.getGreen() + 0.1 * col.getBlue()) / 255;
+		double temp = 1 - (0.3 * currentCol.getRed() + 0.6 * currentCol.getGreen() + 0.1 * currentCol.getBlue()) / 255;
 		if(temp < 0.5)
-			balance = 0; // bright colour = dark font
-		else
-			balance = 255; // dark colour = light font
-		return new Color(balance, balance, balance);
+			return new Color(0, 0, 0); // bright colour = dark font
+		return new Color(255, 255, 255); //dark colour = light font
 	}
 
-	private String getColourString(Color current)
+	/**
+	 * allows the interface to show a textual representation of the current colour the mouse is on
+	 * @return a string containing the hexadecimal and rgb representation of the current colour.
+	 *  The string is formatted using html to allow for multiple lines in one component (e.g. a JLabel with multiple line) */
+	private String getColourString()
 	{
-		return "<html>#" + Integer.toHexString(current.getRed()) + Integer.toHexString(current.getGreen()) + Integer.toHexString(current.getBlue())
-	    + "<br> R: " + Integer.toString(current.getRed()) + "<br> G: " + Integer.toString(current.getGreen()) + "<br> B: " + Integer.toString(current.getBlue()) + "</html>";
+		StringBuilder finalText = new StringBuilder();
+
+		int red = currentCol.getRed();
+		int green = currentCol.getGreen();
+		int blue = currentCol.getBlue();
+
+	    String redHex = Integer.toHexString(red);
+		String greenHex = Integer.toHexString(green);
+		String blueHex = Integer.toHexString(blue);
+
+		// the following makes sure each hex part of the colour is 2 chars long
+		// without this the parts may be missing a leading "0" after being converted to the hex string
+		if(redHex.length() != 2)
+			redHex = "0" + redHex;
+		if(greenHex.length() != 2)
+			greenHex = "0" + greenHex;
+		if(blueHex.length() != 2)
+			blueHex = "0" + blueHex;
+		finalText.append("<html>#" + redHex + greenHex + blueHex + "<br>R: " + red + "<br>G: " + green + "<br>B: " + blue + "</html>");
+
+		return finalText.toString();
 	}
 
 	/**
@@ -93,13 +126,13 @@ public class Pickasso
 	 * @return current mouse X position */
 	private int getCurrentX()
 	{
-		return (int)MouseInfo.getPointerInfo().getLocation().getX();
+		return (int)mousePos.getX();
 	}
 
 	/**
 	 * @return current mouse Y position */
 	private int getCurrentY()
 	{
-		return (int)MouseInfo.getPointerInfo().getLocation().getY();
+		return (int)mousePos.getY();
 	}
 }
